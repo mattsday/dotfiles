bfb582c8e94dc96dfab449fb32674b4e743a8aca
Matt Day <mattsday@gmail.com>
2016-03-25 09:29:42 +0000

    Changed default editor from vim to neovim if available

--- init.sh
+++ init.sh
@@ -22,3 +22,9 @@ done
 if [[ -d $HOME/.ssh ]]; then
 	ln -fs $PWD/ssh_config $HOME/.ssh/config
 fi
+
+# Add nvim config file (same as vimrc):
+if [[ ! -d $HOME/.config/nvim ]]; then
+	mkdir -p $HOME/.config/nvim
+fi
+ln -fs $PWD/vimrc $HOME/.config/nvim/init.vim
