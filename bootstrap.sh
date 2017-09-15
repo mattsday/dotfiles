c6af39de2d333b93438f3e15436442f3d87c7499
Matt Day <mattsday@gmail.com>
2017-09-15 20:23:25 +0100

    Meh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ fi
 SYSTEM=`uname`
 if [ "$SYSTEM" = Darwin ]; then
 	if [ -f ./mac-bootstrap.sh ]; then
+		echo Detected Mac OS X
 		./mac-bootstrap.sh
 		exit
 	fi
@@ -17,6 +18,7 @@ fi
 # Maybe Debian?
 if [ -f "/etc/debian_version" ] && [ -x "/usr/bin/apt-get" ]; then
 	if [ -f ./debian-bootstrap.sh ]; then
+		echo Detected Debian flavoured
 		./debian-bootstrap.sh
 		exit
 	fi
@@ -27,6 +29,7 @@ if [ -f "/etc/os-release" ]; then
 	RELEASE=`cat /etc/os-release | grep '^ID=' | awk -F= '{print $2}' | sed 's/"//g'`
 	if [ "$RELEASE" = centos ] || [ $RELEASE = rhel ] || [ $RELEASE = fedora ]; then
 		if [ -f ./centos-bootstrap.sh ]; then
+			echo Detected Red Hat flavoured
 			./centos-bootstrap.sh
 			exit
 		fi
