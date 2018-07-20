b187707b46d81d31c09a7548b96723b27c5fa7d0
Matt Day <mattsday@gmail.com>
2018-07-20 08:43:50 +0100

    Fixed removal to check for links (safer)

--- remove.sh
+++ remove.sh
@@ -8,7 +8,7 @@ echo Removing dotfiles
 # Figure out what we've got linked
 for dotfile in ${dotfiles}; do
 	FILE="$HOME/.${dotfile}"
-	if [ -f "${FILE}" ]; then
+	if [ -L "${FILE}" ]; then
 		echo Removing "${FILE}"
 		rm "${FILE}"
 	fi
@@ -21,7 +21,7 @@ done
 
 for dotfile in ${extra}; do
 	FILE="${dotfile}"
-	if [ -f "${FILE}" ]; then
+	if [ -L "${FILE}" ]; then
         echo Removing "${FILE}"
 		rm "${FILE}"
     fi
@@ -35,22 +35,20 @@ done
 if [ -f "${USER_CHROME}" ]; then
 	echo Removing "${USER_CHROME}"
 	rm "${USER_CHROME}"
-fi
-
-if [ -f "${PWD}/backup/userChrome.css" ]; then
-	echo Restoring user chrome from backup
-	mv "${PWD}/backup/userChrome.css" "${USER_CHROME}"
+	if [ -L "${PWD}/backup/userChrome.css" ]; then
+		echo Restoring user chrome from backup
+		mv "${PWD}/backup/userChrome.css" "${USER_CHROME}"
+	fi
 fi
 
 VS_SETTINGS="$VS_DIR/settings.json"
-if [ -f "${VS_SETTINGS}" ]; then
+if [ -L "${VS_SETTINGS}" ]; then
 	echo Removing "${VS_SETTINGS}"
 	rm "${VS_SETTINGS}"
-fi
-
-if [ -f "${PWD}/backup/local_settings.json" ]; then
-	echo Restoring "$VS_SETTINGS" from backup
-	mv "backup/local_settings.json" "$VS_SETTINGS"
+	if [ -f "${PWD}/backup/local_settings.json" ]; then
+		echo Restoring "$VS_SETTINGS" from backup
+		mv "backup/local_settings.json" "$VS_SETTINGS"
+	fi
 fi
 
 echo Done
