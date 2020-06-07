8e678ef99fe9ad3b8ecfc984f361d9aad43cc087
Matt Day <mattsday@google.com>
2020-06-07 16:50:53 +0100

    Made ferdi script idempotent

--- ferdi-shopping-list.sh
+++ ferdi-shopping-list.sh
@@ -2,6 +2,7 @@
 
 BASE_DIR="${HOME}/.config/Ferdi/recipes/dev"
 GIT_LOCATION="https://github.com/sharkpp/franz-recipe-google-shoppinglist"
+PLUGIN_DIR="google-shopping-list"
 
 error() {
     >&2 echo '[Error]' "$@"
@@ -31,7 +32,12 @@ main() {
 
     cd "${BASE_DIR}" || error "Could not change directory to ${BASE_DIR}"
 
-    git clone "${GIT_LOCATION}" google-shopping-list || error "Could not clone git project"
+    if [[ ! -d "${BASE_DIR}/${PLUGIN_DIR}" ]]; then
+        git clone "${GIT_LOCATION}" "${PLUGIN_DIR}" || error "Could not clone git project"
+    else
+        cd "${BASE_DIR}/${PLUGIN_DIR}" || error "Could not change directory to ${BASE_DIR}/${PLUGIN_DIR}"
+        git pull || error "Could not run a git pull"
+    fi
 
     info Installation successful
     
