4bcd4e366c513fbcda842495a3fa2ade0cad9832
mattsday <mattsday@google.com>
2020-06-10 17:48:36 +0100

    Added docker and spectacle to rodete bootstrap

--- rodete-bootstrap.sh
+++ rodete-bootstrap.sh
@@ -14,47 +14,93 @@ echo '[Info]' "$@"
 }
 
 
-test_root() {
-if [ "$(id -u)" -ne 0 ]; then
-    if command -v sudo >/dev/null 2>&1; then
-        warn This must be run as root - attempting to use sudo
-        if ! sudo "$0" "$@"; then
-            fail This must be run as root - e.g. "'sudo $0'"
-        fi
-        # Was successful as sudo so just exit
-        exit
-    fi
-    fail This must be run as root
-fi
-}
-
 install_apt_packages() {
 APT_PACKAGES=(snapd kde-plasma-desktop plasma-widgets-addons plasma-wallpapers-addons)
 APT_PACKAGES+=(libffmpegthumbnailer4v5 ffmpegthumbs ffmpegthumbnailer pulseaudio-module-bluetooth)
-APT_PACKAGES+=(spotify-client sddm-theme-debian-breeze)
-
-apt -y install "${APT_PACKAGES[@]}"
+APT_PACKAGES+=(spotify-client sddm-theme-debian-breeze blueman google-cloud-sdk google-cloud-sdk-anthos-auth)
+APT_PACKAGES+=(google-cloud-sdk-kpt google-cloud-sdk-skaffold kubectl openjdk-8-jdk openjdk-11-jdk)
+APT_PACKAGES+=(gnucash kde-spectacle)
+echo Installing packages "${APT_PACKAGES[@]}"
+sudo apt-get -y install "${APT_PACKAGES[@]}" >/dev/null || fail "Failed installing packages"
 }
 
 install_snaps() {
-snap install chromium
-snap install code --classic
-snap install intellij-idea-ultimate --classic
+sudo snap install chromium >/dev/null || warn "Failed to install chromium"
+sudo snap install code --classic >/dev/null || warn "Failed to install code"
+sudo snap install intellij-idea-ultimate --classic >/dev/null || warn "Failed to install intellij-idea-ultimate"
 }
 
 passwordless_sudo() {
-echo 'mattsday ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/nopasswd
-AUTOMATIC_UPDATE=yes glinux-config set custom_etc_sudoers_d true
+if [ ! -f /etc/sudoers.d/nopasswd ]; then
+echo 'mattsday ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/nopasswd
+fi
+sudo AUTOMATIC_UPDATE=yes glinux-config set custom_etc_sudoers_d true
+}
+
+bluetooth_setup() {
+if [ ! -f "$HOME/.config/pulse/default.pa" ]; then
+cat > "$HOME/.config/pulse/default.pa" <<EOF
+.include /etc/pulse/default.pa
+
+# Switch all audio playback to Bluetooth headphones when they are connected
+.ifexists module-switch-on-connect.so
+load-module module-switch-on-connect
+.endif
+
+# Enable automatic switching between A2DP and HSP/HFP
+.ifexists module-bluetooth-policy.so
+unload-module module-bluetooth-policy
+load-module module-bluetooth-policy auto_switch=2
+.endif
+
+# Fix bluetooth headphones bug
+.ifexists module-bluetooth-discover.so
+unload-module module-bluetooth-discover
+load-module module-bluetooth-discover
+.endif
+EOF
+fi
+}
+
+docker_setup() {
+echo Setting up Docker
+sudo glinux-add-repo -b docker-ce-"$(lsb_release -cs)" >/dev/null || fail Failed to add Docker repo
+sudo apt-get update >/dev/null || fail Failed to update
+sudo apt-get -y install docker-ce >/dev/null || fail Failed to install Docker
+sudo service docker stop
+sudo ip link set docker0 down
+sudo ip link del docker0
+sudo addgroup docker >/dev/null
+sudo adduser $USER docker >/dev/null
+if [ ! -f /etc/docker/daemon.json ]; then
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "data-root": "/usr/local/google/docker",
+  "bip": "192.168.9.1/24",
+  "default-address-pools": [
+    {
+      "base": "192.168.10.0/24",
+      "size": 24
+    }
+  ],
+  "storage-driver": "overlay2",
+  "debug": true,
+  "registry-mirrors": ["https://mirror.gcr.io"]
+}
+EOF
+fi
+sudo service docker start
+
+
 }
 
 main() {
-test_root
-# Install apt packages
+passwordless_sudo
 install_apt_packages
 install_snaps
-passwordless_sudo
-
+bluetooth_setup
+docker_setup
 }
 
 
-main
+main "$@"
