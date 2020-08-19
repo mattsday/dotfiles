1ae8e4663d03cda63f69b22ae5cd4b51b2541459
Matt Day <mattsday@gmail.com>
2020-08-19 22:44:25 +0100

    Kubectx no longer needs bash

--- os-bootstraps/kubectx.sh
+++ os-bootstraps/kubectx.sh
@@ -1,20 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
 KUBECTX_VERSION=v0.9.1
-KUBECTX_URL=(https://github.com/ahmetb/kubectx/releases/download/"$KUBECTX_VERSION"/kubectx https://github.com/ahmetb/kubectx/releases/download/"$KUBECTX_VERSION"/kubens)
-KUBECTX_ZSH_COMPLETION=(https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.zsh https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.zsh)
-KUBECTX_BASH_COMPLETION=(https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.bash)
-KUBECTX_FISH_COMPLETION=(https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.fish https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.fish)
+KUBECTX_URL="https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubectx https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubens"
+KUBECTX_ZSH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.zsh https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.zsh"
+KUBECTX_BASH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.bash"
+KUBECTX_FISH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.fish https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.fish"
 ZSH_COMPLETION_DIR=/usr/share/zsh/vendor-completions
-BASH_COMPLETION_DIR="$(pkg-config --variable=completionsdir bash-completion)"
 FISH_COMPLETION_DIR="$HOME/.config/fish/completions"
+if command -v pkg-config >/dev/null 2>&1; then
+    BASH_COMPLETION_DIR="$(pkg-config --variable=completionsdir bash-completion)"
+elif [ -d /usr/local/etc/bash_completion.d ]; then
+    BASH_COMPLETION_DIR=/usr/local/etc/bash_completion.d/
+else
+    BASH_COMPLETION_DIR=/etc/bash_completion.d
+fi
 
 fail() {
     echo "$@"
     exit 1
 }
 
-for url in "${KUBECTX_URL[@]}"; do
+if [ ! -d "$BASH_COMPLETION_DIR" ]; then
+    sudo mkdir -p "$BASH_COMPLETION_DIR" || fail Could not create "$BASH_COMPLETION_DIR" 
+fi
+
+for url in $KUBECTX_URL; do
     filename="$(basename "$url" | xargs)"
     curl -sL --fail "$url" | sudo tee /usr/local/bin/"$filename" >/dev/null || fail Download failed
     sudo chmod +x /usr/local/bin/"$filename" || fail Chmodding "$filename" failed
@@ -32,7 +42,7 @@ if [ ! -d "$FISH_COMPLETION_DIR" ]; then
     mkdir -p "$FISH_COMPLETION_DIR" || fail Cannot create "$FISH_COMPLETION_DIR"
 fi
 
-for url in "${KUBECTX_ZSH_COMPLETION[@]}"; do
+for url in $KUBECTX_ZSH_COMPLETION; do
     filename="$ZSH_COMPLETION_DIR"/_"$(basename "$url" | xargs)"
     if [ -f "$filename" ]; then
         sudo rm "$filename" || fail Cannot remove "$filename"
@@ -40,7 +50,7 @@ for url in "${KUBECTX_ZSH_COMPLETION[@]}"; do
     curl -sL --fail "$url" | sudo tee "$filename" >/dev/null || fail Download failed for "$url"
 done
 
-for url in "${KUBECTX_BASH_COMPLETION[@]}"; do
+for url in $KUBECTX_BASH_COMPLETION; do
     filename="$BASH_COMPLETION_DIR"/"$(basename "$url" | xargs | awk -F. '{print $1}')"
     if [ -f "$filename" ]; then
         sudo rm "$filename" || fail Cannot remove "$filename"
@@ -48,7 +58,7 @@ for url in "${KUBECTX_BASH_COMPLETION[@]}"; do
     curl -sL --fail "$url" | sudo tee "$filename" >/dev/null || fail Download failed for "$url"
 done
 
-for url in "${KUBECTX_FISH_COMPLETION[@]}"; do
+for url in $KUBECTX_FISH_COMPLETION; do
     filename="$FISH_COMPLETION_DIR"/"$(basename "$url" | xargs)"
     if [ -f "$filename" ]; then
         rm "$filename" || fail Cannot remove "$filename"
