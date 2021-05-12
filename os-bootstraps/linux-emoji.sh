a894e91b20fce312df0e43df68650542d3403e40
mattsday <mattsday@google.com>
2021-05-12 09:57:48 +0100

    Improved linux emoji setup performance

--- os-bootstraps/linux-emoji.sh
+++ os-bootstraps/linux-emoji.sh
@@ -1,30 +1,63 @@
 #!/bin/sh
+# Sets up noto colour emoji as the default emoji font
+# This in turn makes emojis so much better in e.g. Konsole, notifications, etc
+
+fail() {
+  echo >&2 '[Failure]' "$@"
+  exit 1
+}
+
+warn() {
+  echo >&2 '[Warning]' "$@"
+}
+
 echo Setting up linux emoji
-if [ -x /usr/bin/apt-get ]; then
-    DEBIAN_FRONTEND="noninteractive" sudo apt-get "$@" -y install fonts-noto'*' >/dev/null
-else
-    echo Note - you should install fonts-noto-'*' after this
+
+# Check if the correct fonts are installed and install them on debian-based distros
+if [ -x /usr/bin/apt-get ] && [ -x /usr/bin/dpkg ]; then
+    if ! dpkg-query -W -f='${Status}' fonts-noto 2>/dev/null | grep "ok installed" >/dev/null 2>&1; then
+        DEBIAN_FRONTEND="noninteractive" sudo apt-get "$@" -y install fonts-noto fonts-noto'*' >/dev/null
+    fi
+elif ! fc-list 2>/dev/null | grep NotoColorEmoji >/dev/null 2>&1; then
+    echo Note - install fonts-noto-'*' after this
 fi
 
+# Check if fontconfig dir is a symlink and if so remove it
 if [ -L "${HOME}"/.config/fontconfig ]; then
-    rm "${HOME}"/.config/fontconfig
+    rm "${HOME}"/.config/fontconfig || fail Cannot unlink "${HOME}"/.config/fontconfig
 fi
 
+# Create the dir if needed
 if [ ! -d "${HOME}"/.config/fontconfig ]; then
-    mkdir -p "${HOME}"/.config/fontconfig
-fi
-
-if [ -f "${HOME}"/.config/fontconfig/fonts.conf ] && [ ! -L "${HOME}"/.config/fontconfig/fonts.conf ]; then
-    echo Backing up local fonts.conf
-    cp "${HOME}"/.config/fontconfig/fonts.conf "${PWD}"/backup/local-fonts.conf
+    mkdir -p "${HOME}"/.config/fontconfig || fail Cannot create dir "${HOME}"/.config/fontconfig
 fi
 
 SOURCE_FILE="${PWD}"/dotfiles/special/fontconfig/fonts.conf
 if [ ! -f "${SOURCE_FILE}" ]; then
     BASE="$(dirname "${PWD}" | xargs)"
     SOURCE_FILE="${BASE}"/dotfiles/special/fontconfig/fonts.conf
+    if [ ! -f "${SOURCE_FILE}" ]; then
+        fail Cannot locate "${SOURCE_FILE}"
+    fi
 fi
 
-ln -fs "${SOURCE_FILE}" "${HOME}"/.config/fontconfig/fonts.conf >/dev/null
+DESTINATION_FILE="${HOME}"/.config/fontconfig/fonts.conf
 
-fc-cache -f -v >/dev/null
+# Check if the file is already symlinked, if so we can exit
+if [ -L "${DESTINATION_FILE}" ] && EXISTING_FONTS_FILE=$(readlink "${HOME}"/.config/fontconfig/fonts.conf) 2>/dev/null; then
+    if [ ! -L "${EXISTING_FONTS_FILE}" ]; then
+        if [ "${EXISTING_FONTS_FILE}" = "${SOURCE_FILE}" ]; then
+            # Don't bother continuing and running the expensive fc-cache command
+            exit 0
+        fi
+    fi
+fi
+
+if [ -f "${DESTINATION_FILE}" ] && [ ! -L "${DESTINATION_FILE}" ]; then
+    echo Backing up local fonts.conf 
+    cp "${DESTINATION_FILE}" "${PWD}"/backup/local-fonts.conf  || warn Cannot backup "${DESTINATION_FILE}" to "${PWD}"/backup/local-fonts.conf
+fi
+
+ln -fs "${SOURCE_FILE}" "${DESTINATION_FILE}" || fail Cannot copy "${SOURCE_FILE}" to "${DESTINATION_FILE}" 
+
+fc-cache -f -v >/dev/null || warn fc-cache had errors
