693ef3f79fddb6b8700ee9a114d04becf0af227c
Matt Day <mattsday@google.com>
2021-05-30 08:15:45 +0100

    kubectx now downloads Go binary

--- os-bootstraps/kubectx.sh
+++ os-bootstraps/kubectx.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
-KUBECTX_VERSION=v0.9.1
-KUBECTX_URL="https://github.com/ahmetb/kubectx/releases/download/${KUBECTX_VERSION}/kubectx https://github.com/ahmetb/kubectx/releases/download/${KUBECTX_VERSION}/kubens"
+[ -z "${KUBECTX_VERSION}" ] && KUBECTX_VERSION=v0.9.3
+[ -z "${KUBECTX_ARCH}" ] && KUBECTX_ARCH=linux_x86_64
+[ -z "${TMP_DIR}" ] && TMP_DIR=/tmp
+[ -z "${KUBECTX_PREFIX}" ] && KUBECTX_PREFIX=/usr/local/bin
+
+if [ -z "${KUBECTX_FORCE}" ] && command -v kubectx >/dev/null 2>&1 && command -v kubens >/dev/null 2>&1; then
+    echo Kubectx and Kubens already installed, set KUBECTX_FORCE=1 to force
+    exit 0
+fi
+
+KUBECTX_FILE=kubectx_"${KUBECTX_VERSION}"_"${KUBECTX_ARCH}".tar.gz
+KUBENS_FILE=kubens_"${KUBECTX_VERSION}"_"${KUBECTX_ARCH}".tar.gz
+
+KUBECTX_BASE_URL="https://github.com/ahmetb/kubectx/releases/download/${KUBECTX_VERSION}"
+
 KUBECTX_ZSH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.zsh https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.zsh"
 KUBECTX_BASH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.bash https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.bash"
 KUBECTX_FISH_COMPLETION="https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.fish https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.fish"
+
 ZSH_COMPLETION_DIR=/usr/share/zsh/vendor-completions
 FISH_COMPLETION_DIR="${HOME}/.config/fish/completions"
+
 if command -v pkg-config >/dev/null 2>&1; then
     BASH_COMPLETION_DIR="$(pkg-config --variable=completionsdir bash-completion)"
 elif [ -d /usr/local/etc/bash_completion.d ]; then
@@ -20,15 +35,36 @@ fail() {
     exit 1
 }
 
-if [ ! -d "${BASH_COMPLETION_DIR}" ]; then
-    sudo mkdir -p "${BASH_COMPLETION_DIR}" || fail Could not create "${BASH_COMPLETION_DIR}" 
-fi
+download() {
+    filename="$1"
+    output_file="$2"
+    if [ -z "$output_file" ] || [ -z "$filename" ]; then
+        fail Files not found
+    fi
+    downloaded_file="${TMP_DIR}/$1"
 
-for url in ${KUBECTX_URL}; do
-    filename="$(basename "${url}" | xargs)"
-    curl -sL --fail "${url}" | sudo tee /usr/local/bin/"${filename}" >/dev/null || fail Download failed
-    sudo chmod +x /usr/local/bin/"${filename}" || fail Chmodding "${filename}" failed
-done
+    url="${KUBECTX_BASE_URL}/${filename}"
+    if [ -f "${downloaded_file}" ]; then
+        rm "${downloaded_file}" || fail "${downloaded_file}" exists and could not be deleted
+    fi
+    curl -sL --fail "${url}" -o "${downloaded_file}" || fail Download of "${url}" failed
+    if [ ! -f "${downloaded_file}" ]; then
+        fail "Download failed (not found in ${downloaded_file})"
+    fi
+    tar zxvf "${downloaded_file}" -C "${TMP_DIR}" >/dev/null 2>&1 || fail Could not extract "${downloaded_file}"
+    if [ -f "${TMP_DIR}/${output_file}" ]; then
+        sudo mv "${TMP_DIR}/${output_file}" "${KUBECTX_PREFIX}"
+    else
+        fail Could not download "${output_file}"
+    fi
+}
+
+download "${KUBECTX_FILE}" kubectx
+download "${KUBENS_FILE}" kubens
+
+if [ ! -d "${BASH_COMPLETION_DIR}" ]; then
+    sudo mkdir -p "${BASH_COMPLETION_DIR}" || fail Could not create "${BASH_COMPLETION_DIR}"
+fi
 
 if [ ! -d "${ZSH_COMPLETION_DIR}" ]; then
     sudo mkdir -p "${ZSH_COMPLETION_DIR}" || fail Cannot create "${ZSH_COMPLETION_DIR}"
