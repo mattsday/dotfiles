88cde0c6964c5d85cc987132fc739f339ab3f68d
Matt Day <mattsday@gmail.com>
2021-06-12 13:03:47 +0100

    Removed lsb-release dependency

--- os-bootstraps/trillian-bootstrap.sh
+++ os-bootstraps/trillian-bootstrap.sh
@@ -71,7 +71,7 @@ EOF
     # Install upstream Docker
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  $(grep VERSION_CODENAME /etc/os-release | cut -f 2 -d =) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
     APT_PACKAGES=(docker-ce docker-ce-cli containerd.io)
     install_apt_packages
