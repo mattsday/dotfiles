558ded5969f192322e9fbed7b17093d04892b38b
Matt Day <mattsday@gmail.com>
2022-02-01 12:30:27 +0000

    Updated spotify scaling to 1.5x

--- hidpi/setup.sh
+++ hidpi/setup.sh
@@ -17,7 +17,7 @@ EOF
     # Spotify client doesn't respect external scale factors, do so here
     if [ -f /usr/share/spotify/spotify.desktop ] && [ ! -f "${HOME}/.local/share/applications/spotify.desktop" ]; then
         cp /usr/share/spotify/spotify.desktop "${HOME}/.local/share/applications/" || error "Cannot move spotify config"
-        sed -i 's/Exec=spotify %U/Exec=spotify --force-device-scale-factor=2 %U/' "${HOME}/.local/share/applications/spotify.desktop"
+        sed -i 's/Exec=spotify %U/Exec=spotify --force-device-scale-factor=1.5 %U/' "${HOME}/.local/share/applications/spotify.desktop"
     fi
 }
 main
