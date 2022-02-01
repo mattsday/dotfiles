f19a899bdde4de6113a09372968bbefaedecf0bb
Matt Day <mattsday@gmail.com>
2022-02-01 10:55:18 +0000

    Added spotify client config

--- hidpi/setup.sh
+++ hidpi/setup.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-if [ ! -f /etc/profile.d/gdk-scale.sh ]; then
-cat <<EOF | sudo tee /etc/profile.d/gdk-scale.sh
+
+error() {
+    echo >&2 '[Error]' "$@"
+    exit 1
+}
+
+main() {
+    # Set GDK global scaling
+    if [ ! -f /etc/profile.d/gdk-scale.sh ]; then
+        cat <<EOF | sudo tee /etc/profile.d/gdk-scale.sh
 #!/bin/sh
 export GDK_DPI_SCALE=1.5
 EOF
-fi
+    fi
+
+    # Spotify client doesn't respect external scale factors, do so here
+    if [ -f /usr/share/spotify/spotify.desktop ] && [ ! -f "${HOME}/.local/share/applications/spotify.desktop" ]; then
+        cp /usr/share/spotify/spotify.desktop "${HOME}/.local/share/applications/" || error "Cannot move spotify config"
+        sed -i 's/Exec=spotify %U/Exec=spotify --force-device-scale-factor=2 %U/' "${HOME}/.local/share/applications/spotify.desktop"
+    fi
+}
+main
